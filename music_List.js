419a5bd2f225cf5b49cd124faa7969df7f23fb6b
ATHUL THOMAS P <147591846+athulthomas4@users.noreply.github.com>
2023-11-14 15:37:23 +0530

    Add files via upload

---
+++ music_List.js
@@ -0,0 +1,242 @@
+let allMusic = [
+    {
+        name: "Na Roja Nuvve",
+        artist: "Kushi",
+        img: "Na-Roja-Nuvve",
+        src: "Na-Roja-Nuvve"
+    }, 
+    {
+        name: "Thinkal Poovin Ithalaval",
+        artist: "Pachuvum Athbutha Vilakkum",
+        img: "Thinkal-Poovin",
+        src: "Thinkal-Poovin"
+    }, 
+    {
+        name: "Adi pennae oru murai",
+        artist: "Adi Penne",
+        img: "Adi-Penne",
+        src: "Adi-Penne"
+    }, 
+    {
+        name: "Kadavul Thandha Kavithai",
+        artist: "Yugi",
+        img: "Kadavul-Thantha",
+        src: "Kadavul-Thantha"
+    }, 
+    {
+        name: "Anbae peranbae",
+        artist: "NGK",
+        img: "Anbae-Peranbae",
+        src: "Anbae-Peranbae"
+    }, 
+    {
+        name: "Naan Gaali",
+        artist: "Good Night",
+        img: "Naan-Gaali",
+        src: "Naan-Gaali"
+    },
+    {
+        name: "The Hymn Of Dharma",
+        artist: "777 Charlie ",
+        img: "The-Hymn-Of-Dharma",
+        src: "The-Hymn-Of-Dharma"
+    },
+    {
+        name: "Mudhal Nee Mudivum Nee",
+        artist: "Mudhal Nee Mudivum Nee",
+        img: "Mudhal-Nee-Mudivum-Nee",
+        src: "Mudhal-Nee-Mudivum-Nee"
+    },  
+    {
+        name: "Aradhya Aradhya",
+        artist: "Kushi",
+        img: "Aradhya",
+        src: "Aradhya"
+    },   
+    {
+        name: "Oke oka lokam nuvve",
+        artist: "Sashi",
+        img: "Okey-Oka-Lokam",
+        src: "Okey-Oka-Lokam"
+    },  
+    {
+        name: "Railin Oligal",
+        artist: "Blue Star ",
+        img: "Railin-Oligal-Blue-Star",
+        src: "Railin-Oligal-Blue-Star"
+    },  
+    {
+        name: "Tere Vaaste Falak Se",
+        artist: "Zara Hatke Zara Bachke",
+        img: "Tere-Vaaste",
+        src: "Tere-Vaaste"
+    },  
+    {
+        name: "Koodamela Koodavechi",
+        artist: "Rummy",
+        img: "Koodamela-Koodavechi",
+        src: "Koodamela-Koodavechi"
+    },  
+    {
+        name: "Thoomanju Veena Vazhiye",
+        artist: "18am padi",
+        img: "am-padi",
+        src: "am-padi"
+    }, 
+    {
+        name: "Aattu thottil kootinullil",
+        artist: "Athiran",
+        img: "Aattuthottil",
+        src: "Aattuthottil"
+    }, 
+    {
+        name: "The Life of Ram",
+        artist: "96",
+        img: "music-1",
+        src: "music-1"
+    },  
+    {
+        name: "Aval kuzhal udhirthidum",
+        artist: "Manithan",
+        img: "aval",
+        src: "aval"
+    }, 
+    {
+        name: "Ela malakkaadinullil",
+        artist: "Pathaam Valavu",
+        img: "Elamalakaadinullil",
+        src: "Elamalakaadinullil"
+    },  
+    {
+        name: "Unnale Ennalum En Jeevan",
+        artist: "Theri",
+        img: "En-Jeevan",
+        src: "En-Jeevan"
+    }, 
+    {
+        name: "En nenchoram aval",
+        artist: "Santhosham",
+        img: "En-Nenjoram",
+        src: "En-Nenjoram"
+    }, 
+    {
+        name: "Ente kannil ninakkai orukiya",
+        artist: "Bangalore Days",
+        img: "Ente-Kannil-Ninakkai",
+        src: "Ente-Kannil-Ninakkai"
+    },
+    {
+        name: "Khairiyat Puchon Kabhi",
+        artist: "Chhichhore",
+        img: "khairiyat",
+        src: "khairiyat"
+    },
+    {
+        name: "Nee... Kavithaigala",
+        artist: "Maragadha Naanayam",
+        img: "Nee-Kavithaigala",
+        src: "Nee-Kavithaigala"
+    }, 
+    {
+        name: "Etho Mazhayil Nanavode",
+        artist: "Vijay Superum Pournamiyum",
+        img: "Etho-Mazhayil",
+        src: "Etho-Mazhayil"
+    },
+    {
+        name: "Kaadhal En Kaviye",
+        artist: "Kaadhal En Kaviye",
+        img: "Kaadhal-En-Kaviye",
+        src: "Kaadhal-En-Kaviye"
+    },
+    {
+        name: "ilakkana kavithai",
+        artist: "Banaras",
+        img: "ilakkana-kavithai",
+        src: "ilakkana-kavithai"
+    },  
+    {
+        name: "Kadhaippomaa",
+        artist: "Oh My Kadavule",
+        img: "Kadhaippoma",
+        src: "Kadhaippoma"
+    },
+    {
+        name: "Evo Kaun Chhe Khalasi",
+        artist: "Khalasi",
+        img: "Khalasi",
+        src: "Khalasi"
+    },
+    {
+        name: "Kesariya tera ishq hai",
+        artist: " Brahmāstra",
+        img: "Kesariya",
+        src: "Kesariya"
+    }, 
+    {
+        name: "Maru Vaarthai Paesaathe",
+        artist: "Enai Noki Paayum Thota",
+        img: "Maruvaarthai",
+        src: "Maruvaarthai"
+    },
+    {
+        name: "Konji Paesida Vaenaam",
+        artist: "Sethupathi",
+        img: "Konji-Pesida-Venaam",
+        src: "Konji-Pesida-Venaam"
+    },
+    {
+        name: "Mehabooba mae teri ",
+        artist: "K.G.F: Chapter 2",
+        img: "Mehabooba",
+        src: "Mehabooba"
+    },
+    {
+        name: "neela shalabhame",
+        artist: "Charminar",
+        img: "neela-shalabhame",
+        src: "neela-shalabhame"
+    },
+    {
+        name: "Oru Kathilola Njan",
+        artist: "vettam",
+        img: "Oru-Kathilola-Njan",
+        src: "Oru-Kathilola-Njan"
+    },
+    {
+        name: "Neethanae Neethanaaae",
+        artist: "Mersal",
+        img: "Neethanae",
+        src: "Neethanae"
+    },
+    {
+        name: "Usurae poguthae",
+        artist: "Raavanan",
+        img: "Raavanan-Usure-Pogudhey",
+        src: "Raavanan-Usure-Pogudhey"
+    },
+    {
+        name: "Pookkalae Sattru",
+        artist: "I",
+        img: "Pookkalae-Sattru-Oyivedungal",
+        src: "Pookkalae-Sattru-Oyivedungal"
+    },
+    {
+        name: "tum hi ho",
+        artist: "Aashiqui 2",
+        img: "tum-hi-ho",
+        src: "tum-hi-ho"
+    },
+    {
+        name: "Yaathonnum Parayathe",
+        artist: "Vaashi",
+        img: "Yaathonnum-Parayathe",
+        src: "Yaathonnum-Parayathe"
+    },
+    {
+        name: "Ee Kaattu Vnnu Kaathil",
+        artist: "Adam Joan",
+        img: "Ee-Kaattu-Song",
+        src: "Ee-Kaattu-Song"
+    },
+]
